dbd14d3100988e81fe2607cc73f65d94548b7550
Cassandre Tirions <ctirions@fu-r8-p8.s19.be>
2022-06-13 16:40:55 +0200

    Element access and modifiers

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: ctirions <ctirions@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 11:07:28 by ctirions          #+#    #+#             */
-/*   Updated: 2022/06/08 18:55:07 by ctirions         ###   ########.fr       */
+/*   Updated: 2022/06/13 16:40:12 by ctirions         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -120,11 +120,114 @@ namespace ft {
 		size_type	max_size(void) const { return (this->_alloc.max_size()); }
 
 		void	resize(size_type n, value_type val = value_type()) {
-			if (!n)
-				return ;
+			if (n < this->_size)
+				while (n < this->_size)
+					this->_alloc.destroy(&this->_value[this->_size--]);
+			else if (n > this->_size && n > this->_capacity)
+				this->reserve(n);
+			if (n > this->_size)
+				while (this->_size < n)
+					this->_alloc.construct(&this->_value[this->_size++], val);
 		}
-		/*-----  -----*/
-		/*-----  -----*/
+
+		size_type	capacity(void) const { return (this->_capacity); }
+
+		bool empty(void) const { return (!this->_size); }
+
+		void	reserve(size_type n) {
+			if (n > this->max_size())
+				throw (std::out_of_range("vector: out of range"));
+			if (n < this->_capacity)
+				return ;
+
+			pointer old_value = this->_value;
+			this->_value = this->_alloc.allocate(n);
+			for (int i = 0; i < this->_size; i++)
+				this->_alloc.construct(&this->_value[i], old_value[i]);
+			for	(int i = 0; i < this->_size; i++)
+				this->_alloc.destroy(&old_value[i]);
+			this->_alloc.deallocate(old_value, this->_capacity);
+			this->_capacity = n;
+
+		}
+
+		/*----- Element access -----*/
+
+		reference	operator[](size_type pos) const {
+			if (pos > this->_size || pos < 0)
+				throw (std::out_of_range("vector: out of range"));
+			return (this->_value[pos]);
+		}
+
+		const_reference	operator[](size_type pos) const {
+			if (pos > this->_size || pos < 0)
+				throw (std::out_of_range("vector: out of range"));
+			return (this->_value[pos]);
+		}
+
+		reference	at(size_type pos) const {
+			if (pos > this->_size || pos < 0)
+				throw (std::out_of_range("vector: out of range"));
+			return (this->_value[pos]);
+		}
+
+		const_reference	at(size_type pos) const {
+			if (pos > this->_size || pos < 0)
+				throw (std::out_of_range("vector: out of range"));
+			return (this->_value[pos]);
+		}
+
+		reference	front(void) const {
+			if (!this->_size)
+				throw (std::out_of_range("vector: out of range"));
+			return (this->_value[0]);
+		}
+
+		const_reference	front(void) const {
+			if (!this->_size)
+				throw (std::out_of_range("vector: out of range"));
+			return (this->_value[0]);
+		}
+
+		reference	back(void) const {
+			if (!this->_size)
+				throw (std::out_of_range("vector: out of range"));
+			return (this->_value[this->_size - 1]);
+		}
+
+		const_reference	back(void) const {
+			if (!this->_size)
+				throw (std::out_of_range("vector: out of range"));
+			return (this->_value[this->_size - 1]);
+		}
+
+		/*----- Modifiers -----*/
+
+		template <class InputIterator>
+		void	assign(InputIterator first, InputIterator last) {
+			int	i = 0;
+			InputIterator tmp = first;
+			while (tmp++ != last)
+				i++;
+			this->resize(0);
+			this->reserve(i);
+			this->_size = i;
+			i = 0;
+			while (first != last) {
+				this->_alloc.construct(&this->_value[i], *first);
+				i++;
+				first++;
+			}
+		}
+
+		void	assign(size_type n, const_reference val) {
+			this->resize(0);
+			this->reserve(n);
+			for (int i = 0; i < n; i++)
+				this->_alloc.construct(&this->_value[i], val);
+			this->_size = n;
+		}
+
 		/*-----  -----*/
 		/*-----  -----*/
 		/*-----  -----*/
