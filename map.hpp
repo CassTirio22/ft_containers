3d5b05323b3a4f6de960c3ca096c1678aacaf0e0
Cassandre Tirions <ctirions@fu-r8-p6.s19.be>
2022-06-20 21:08:50 +0200

    begin of map

--- map.hpp
+++ map.hpp
@@ -1,22 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_containers.hpp                                  :+:      :+:    :+:   */
+/*   map.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ctirions <ctirions@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/03 11:12:47 by ctirions          #+#    #+#             */
-/*   Updated: 2022/06/20 16:32:26 by ctirions         ###   ########.fr       */
+/*   Created: 2022/06/20 21:04:47 by ctirions          #+#    #+#             */
+/*   Updated: 2022/06/20 21:08:18 by ctirions         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_CONTAINERS_HPP
-# define FT_CONTAINERS_HPP
+#ifndef MAP_HPP
+# define  MAP_HPP
 
-# include <iostream>
-# include <memory>
-# include <cstddef>
-
-# include "utils.hpp"
+namespace ft {
+	template <class Key, class T, class Compare = less<Key>, class Alloc = allocator<ft::pair<const Key, T> > >
+	class map {}
+}
 
 #endif
